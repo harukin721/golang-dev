a029944bdf760d7c6ac2ebe0df1e6871855e3923
harukin <harukin@pepabo.com>
2023-06-11 20:40:30 +0900

    リファクタリング
    - テストを書く
    - コンパイラをパスする
    - テストを実行し、失敗することを確認し、エラーメッセージが意味があることを確認
    - テストに合格するのに十分なコードを記述する
    - リファクタリング

--- main_test.go
+++ main_test.go
@@ -2,22 +2,22 @@ package main
 
 import "testing"
 
-// テスト関数は `Test` で始める
 func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// ヘルパー関数については、 *testing.T と *testing.B の両方が満たすインターフェース
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
